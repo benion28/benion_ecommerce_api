2fba6e6f43053d56c4a29ec40a880277de70692e
Bernard Iorver <53957336+benion28@users.noreply.github.com>
2025-06-01 12:00:25 +0100

    Initial Commit

---
+++ src/main/java/com/benion/benion_ecommerce_api/BenionEcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.benion.benion_ecommerce_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenionEcommerceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BenionEcommerceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/benion/benion_ecommerce_api/BenionEcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benion.benion_ecommerce_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenionEcommerceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
