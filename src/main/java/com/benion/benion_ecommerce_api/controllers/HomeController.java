abce900badc99b7e480ff054f57dd865f17b24bd
Bernard Iorver <53957336+benion28@users.noreply.github.com>
2025-06-22 05:14:34 +0100

    Passing Data to Template

--- src/main/java/com/benion/benion_ecommerce_api/controllers/HomeController.java
+++ src/main/java/com/benion/benion_ecommerce_api/controllers/HomeController.java
@@ -2,21 +2,30 @@ package com.benion.benion_ecommerce_api.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @Value("${spring.application.name}")
     private String appName;
-    @RequestMapping("/")
-    public String index() {
-        System.out.println("Welcome to Home: " + appName);
+    @RequestMapping("/test")
+    public String test() {
+        System.out.println("Welcome to Test: " + appName);
         return "test.html";
+    }
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        String name = "Benion E-commerce API";
+        model.addAttribute("name", name);
+        System.out.println("Welcome to Home: " + name);
+        return "index";
     }
 
     @RequestMapping("/hello")
     public String sayHello() {
-        System.out.println("Welcome to SayHello: " + appName);
+        System.out.println("Welcome to Hello: " + appName);
         return "test.html";
     }
 }
