1974000f856a3fe83ebf0de8c88c1a9c4ec0358b
Bernard Iorver <53957336+benion28@users.noreply.github.com>
2025-06-22 05:59:12 +0100

    Http Status Code

--- src/main/java/com/benion/benion_ecommerce_api/controllers/UserController.java
+++ src/main/java/com/benion/benion_ecommerce_api/controllers/UserController.java
@@ -3,6 +3,9 @@ package com.benion.benion_ecommerce_api.controllers;
 import com.benion.benion_ecommerce_api.entities.User;
 import com.benion.benion_ecommerce_api.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +23,19 @@ public class UserController {
         return userRepository.findAll();
     }
 
+//    @GetMapping("/{id}")
+//    public User getUser(@PathVariable Long id) {
+//        return userRepository.findById(id).orElse(null);
+//    }
+
     @GetMapping("/{id}")
-    public User getUser(@PathVariable Long id) {
-        return userRepository.findById(id).orElse(null);
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        var user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return ResponseEntity.notFound().build();
+        }
+//        return new ResponseEntity<>(user, HttpStatus.ok);
+        return ResponseEntity.ok(user);
     }
 }
