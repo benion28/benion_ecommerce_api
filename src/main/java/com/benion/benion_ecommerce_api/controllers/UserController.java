e02f239817509228d4a0e10c50bd546eb084a22b
Bernard Iorver <53957336+benion28@users.noreply.github.com>
2025-06-22 05:50:11 +0100

    Creating Dynamic Routes

--- src/main/java/com/benion/benion_ecommerce_api/controllers/UserController.java
+++ src/main/java/com/benion/benion_ecommerce_api/controllers/UserController.java
@@ -4,17 +4,24 @@ import com.benion.benion_ecommerce_api.entities.User;
 import com.benion.benion_ecommerce_api.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
-    // method: GET,
+    @GetMapping
+//    @GetMapping("")
     public Iterable<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
     }
 }
